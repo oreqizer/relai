8acb01511c874adb02e8c016391dc5b90b9ebf56
oreqizer <oreqizer@gmail.com>
2017-10-30 09:34:59 +0100

    Toggle: Adjust styles

--- src/client/scenes/Todos/components/Toggle/index.jsx
+++ src/client/scenes/Todos/components/Toggle/index.jsx
@@ -2,12 +2,6 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const Label = styled.label`
-  background-image: url("data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E");
-  background-repeat: no-repeat;
-  background-position: center left;
-`;
-
 const Checkbox = styled.input`
   text-align: center;
   width: 40px;
@@ -20,6 +14,13 @@ const Checkbox = styled.input`
   border: none; /* Mobile Safari */
   -webkit-appearance: none;
   appearance: none;
+
+  background-image: ${props =>
+    props.checked
+      ? "url('data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E');"
+      : 'url("data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E");'}
+  background-repeat: no-repeat;
+  background-position: center left;
 `;
 
 type Props = {|
@@ -29,9 +30,9 @@ type Props = {|
 |};
 
 const Toggle = (props: Props) => (
-  <Label htmlFor={props.id}>
-    <Checkbox id={props.id} checked={props.checked} onChange={props.onChange} />
-  </Label>
+  <label htmlFor={props.id}>
+    <Checkbox id={props.id} type="checkbox" checked={props.checked} onChange={props.onChange} />
+  </label>
 );
 
 export default Toggle;
